65de5a710ba665731610356494fd30226855eb87
Arjun Munji <arjunmunji@gmail.com>
2020-04-09 02:46:52 +0530

    Revert "add OTP functionality"

--- app/api/sendgrid.py
+++ app/api/sendgrid.py
@@ -3,14 +3,14 @@ from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 
 
+
 meessage = Mail(
-    from_email="from_email@example.com",
-    to_emails="to@example.com",
-    subject="Sending with Twilio SendGrid is Fun",
-    html_content="<strong>and easy to do anywhere, even with Python</strong>",
-)
+    from_email='from_email@example.com',
+    to_emails='to@example.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
 try:
-    sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
     response = sg.send(message)
     print(response.status_code)
     print(response.body)
